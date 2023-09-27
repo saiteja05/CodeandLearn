64adaa9ac0dcb935589c463e2be129328b27f5d2
Sai Teja <teja.boddapati@gmail.com>
2023-09-27 21:08:41 +0530

    details correctred

---
+++ DSList/src/demoerrors.java
@@ -0,0 +1,15 @@
+public class demoerrors
+{
+
+    public static void main(String[] args) {
+
+        System.out.println("x");
+
+        dsakdjkdjdsfj;
+
+        System.out.println("chyfr");
+
+    }
+
+}
+
