7b4f355438111f6ab329990076182a46f6230d68
Sai Teja <teja.boddapati@gmail.com>
2024-11-11 01:36:49 +0530

    Update getDatainfo.py
    fixed errors

--- python/getDatainfo.py
+++ python/getDatainfo.py
@@ -1,6 +1,6 @@
 from pymongo import MongoClient
 
-# uri = "mongodb+srv://<>:<>@cluster0.tcgzn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+# uri = "mongodb+srv://<>:<>@mycluster.mongodb.net/?retryWrites=true&w=majority&appName=StatCollector"
 uri = ""
 
 try:
@@ -20,35 +20,36 @@ try:
         for cname in db.list_collections():
             col_name = cname['name']
             print("collection: ", col_name)
-            print("Total Document Count:", db[(cname['name']) + ""].count_documents({}))
-            stats = db.command("collstats", collection_name)
+            stats = db.command("collstats", col_name)
             data_size = stats.get("size", 0)
             print("size(bytes):", data_size)
-            # isSharded=stats.get("sharded")
-            # print("Sharded?:",isSharded)
+            print(f"Document count: {stats['count']}")
             # Printing Indexes
             if 'idIndex' in cname:
                 id_index_value = cname['idIndex']
                 print("Index:", id_index_value)
-            if isSharded:
-                # Check for sharding keys
-                collection_info = config_db.collections.find_one({"_id": f"{db_name}.{col_name}"})
-                # print(collection_info)
-                if not collection_info or not collection_info.get("key"):
-                    print("The specified collection is not sharded.")
-                else:
-                    shard_key = collection_info["key"]
-                    print(f"Shard Key: {shard_key}")
-                    # Retrieve chunk information from 'config.collections' and 'config.chunks'
-                    shard_distribution = {}
-                    for col in config_db.collections.find({"_id": f"{db_name}.{col_name}"}):
-                        # Calculate shard distribution by counting chunks per shard
-                        for chunk in config_db.chunks.find({"uuid": col['uuid']}):
-                            shard_name = chunk["shard"]
-                            shard_distribution[shard_name] = shard_distribution.get(shard_name, 0) + 1
-                        # Output the shard distribution summary
-                        for shard, chunk_count in shard_distribution.items():
-                            print(f"Shard: {shard}, Number of Chunks: {chunk_count}")
+                print(f"Index size: {stats['totalIndexSize']} bytes")
+                for index_name, size in stats['indexSizes'].items():
+                    print(f" - {index_name}: {size} bytes")
+
+            # Check sfor sharding keys
+            collection_info = config_db.collections.find_one({"_id": f"{db_name}.{col_name}"})
+            # print(collection_info)
+            if not collection_info or not collection_info.get("key"):
+                print("The specified collection is not sharded.")
+            else:
+                shard_key = collection_info["key"]
+                print(f"Shard Key: {shard_key}")
+                # Retrieve chunk information from 'config.collections' and 'config.chunks'
+                shard_distribution = {}
+                for col in config_db.collections.find({"_id": f"{db_name}.{col_name}"}):
+                    # Calculate shard distribution by counting chunks per shard
+                     for chunk in config_db.chunks.find({"uuid": col['uuid']}):
+                        shard_name = chunk["shard"]
+                        shard_distribution[shard_name] = shard_distribution.get(shard_name, 0) + 1
+                    # Output the shard distribution summary
+                     for shard, chunk_count in shard_distribution.items():
+                        print(f"Shard: {shard}, Number of Chunks: {chunk_count}")
 
                     # chunks = config_db.chunks.find({"_id": f"{db_name}.{col_name}"})
                     # #Calculate shard distribution by counting chunks per shard
